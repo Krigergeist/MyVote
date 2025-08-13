3559d198fffa96546e414d82dc07a376be341308
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2025-08-13 14:53:11 +0700

    adding route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CandidateController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ResultController;
+use App\Http\Controllers\VotingController;
+use App\Http\Controllers\NotificationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +17,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+// Candidates
+Route::get('/candidates', [CandidateController::class, 'index'])->name('candidates.manage');
+Route::get('/candidates/add', [CandidateController::class, 'create'])->name('candidates.add');
+Route::post('/candidates/store', [CandidateController::class, 'store'])->name('candidates.store');
+
+// Voting
+Route::get('/voting', [VotingController::class, 'index'])->name('voting');
+
+// Result
+Route::get('/results', [ResultController::class, 'index'])->name('results');
+
+// Notification
+Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications');
 
 Route::get('/', function () {
     return view('welcome');
